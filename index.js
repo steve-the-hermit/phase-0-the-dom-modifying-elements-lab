5bef30dda7d7ce85ae47d86087db9f990019b4c1
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-20 00:25:14 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+// index.js
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+const header = document.querySelector('header');
+header.appendChild(newHeader);
